604baaf2976004d311f2b99f6e418d1fd9b01213
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2025-11-24 15:09:13 -0500

    day 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-export function App() {
-  console.log('Oi');
+import './styles/theme.css';
+import './styles/global.css';
+import { Container } from './components/Container';
 
+export function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
-        incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
-        laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
-        quia rerum!
+      <Container>
+        <section>LOGO</section>
+      </Container>
 
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/Heading.tsx
@@ -0,0 +1,10 @@
+import type React from 'react';
+import styles from './Heading.module.css';
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading(props: HeadingProps) {
+  return <h1 className={styles.heading}>{props.children}</h1>;
+}
